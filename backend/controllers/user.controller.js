31cf248ede59b12dac10395a841fd28546b97d79
user.name like : "uzairarif28@gmail.com <uzairarif28@gmail.com>
2025-05-20 03:24:42 +0500

    routes in process

--- backend/controllers/user.controller.js
+++ backend/controllers/user.controller.js
@@ -3,6 +3,7 @@ import UserModel from "../models/user.model.js";
 // GET /api/users
 export const getUsers = async (req, res) => {
 res.json({ message: "Hello World from get" });
-};
+}
 
 
+// aj gap lia ha kal krunga agay inshaAllah 
